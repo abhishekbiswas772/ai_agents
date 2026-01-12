c9c52878d50a9be334423ad8e50ace55313889cf
Abhishek Biswas <abhishek.biswas@cubastion.com>
2026-01-12 23:10:24 +0530

    fixed

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+from typing import Any, Dict
 from clients.llm_client import LLMClient
 import asyncio
+import click
 
-async def main():
+
+class CLI:
+    def __init__(self):
+        pass
+
+    def run_single(self):
+        pass 
+
+async def run(messages : Dict[str, Any]):
     client = LLMClient()
     async for event in client.chat_completion(
-        messages=[
-            {
-                "role" : "user",
-                "content" : "whats app"
-            }
-        ],
+        messages=messages,
         stream=True
     ):
         print(event)
     print("DONE")
 
+@click.command()
+@click.argument("prompt", required=False)
+def main(prompt : str | None):
+    print(prompt)
+    messages = [{
+        "role" : "user",
+        "content" : prompt
+    }]
+    asyncio.run(run(messages=messages))
+    
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
 
     
