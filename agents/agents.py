57804bb231d83ec16318d166b5531a234522cbb1
Abhishek Biswas <abhishek.biswas@cubastion.com>
2026-01-12 23:28:49 +0530

    fix

---
+++ agents/agents.py
@@ -0,0 +1,10 @@
+
+from typing import AsyncGenerator
+
+
+class Agents:
+    def __init__(self):
+        pass 
+
+    async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
+        pass
---
+++ agents/events.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from enum import Enum
+from dataclasses import dataclass, field
+from typing import Any, Dict
+from clients.response import TokenUsage
+
+class AgentEventType(Enum):
+    #AGENT LIFECYCLE
+    AGENT_START = "agent_start"
+    AGENT_END = "agent_end"
+    AGENT_ERROR = "agent_error"
+
+    # TEXT Streaming 
+    TEXT_DELTA  = "text_delta"
+    TEXT_COMPLETE = "text_complete"
+
+
+@dataclass
+class AgentEvent:
+    type : AgentEventType
+    data : Dict[str, Any] = field(default_factory = Dict)
+
+    @classmethod
+    def agent_start(cls, message : str) -> AgentEvent:
+        return cls(
+            type=AgentEventType.AGENT_START,
+            data = {
+                "message" : message
+            }
+        )
+    
+    @classmethod
+    def agent_end(cls, response : str | None = None, usage: TokenUsage | None = None) -> AgentEvent:
+        return cls(
+            type=AgentEventType.AGENT_END,
+            data = {
+                "response" : response,
+                "usage" : usage.__dict__ if usage else None,
+            }
+        )
+    
+    @classmethod
+    def agent_error(cls, error : str, details: Dict[str, Any] | None = None) -> AgentEvent:
+        return cls(
+            type=AgentEventType.AGENT_END,
+            data = {
+                "error" : error,
+                "details" : details,
+            }
+        )
+    
+    @classmethod
+    def text_delta(cls, content : str) -> AgentEvent:
+        return cls(
+            type=AgentEventType.TEXT_DELTA,
+            data = {
+                "content" : content,
+            }
+        )
+    
+    @classmethod
+    def text_complete(cls, content : str) -> AgentEvent:
+        return cls(
+            type=AgentEventType.TEXT_COMPLETE,
+            data = {
+                "content" : content,
+            }
+        )
+
